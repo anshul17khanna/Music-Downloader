bf51fab2b4377c89d0d179f88892b18f3aa01370
Anshul Khanna <anshul17khanna@gmail.com>
2016-07-20 17:35:01 +0530

    url read

--- download.py
+++ download.py
@@ -1,6 +1,9 @@
 import socket
 import sys
 import argparse
+import os
+import urllib
+import re
 
 REMOTE_SERVER = "www.google.com"
 
@@ -24,6 +27,28 @@ def parse_args():
     )
     args = parser.parse_args()
 
+def get_music_show(show_name):
+    show_page = str(urllib.urlopen("http://www.tunefind.com/show/"+show_name.lower().replace(" ","-")).read())
+    //seasons = list(re.findall(r'/season-', show_page))
+
+
+def get_music(show_name):
+    get_music_show(show_name)
+
+def main():
+    print "Download all the songs of a tv show or movie.\n"
+
+    while True:
+        print "Enter movie/show name :"
+        show_name = raw_input('> ')
+
+        directory = os.path.join(show_name)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        get_music(show_name)
+
 if __name__ == '__main__':
     check_connection()
     parse_args()
+    main()
